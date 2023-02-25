2b71d3b27e1b1182242e9b4457399cb7ea5ffb40
juninhocb <juninhocb@homail.com>
2023-02-25 11:16:50 -0300

    impl: bottom navigation bar at home page

--- lib/controllers/popular_product_controller.dart
+++ lib/controllers/popular_product_controller.dart
@@ -26,7 +26,6 @@ class PopularProductController extends GetxController {
   Future <void> getPopularProductList() async{
     Response response = await popularProductRepo.getPopularProductList();
     if (response.statusCode == 200){
-      print("got products");
       _popularProductList = [];
       _popularProductList.addAll(Product.fromJson(response.body).products as Iterable);
       _isLoaded = true;
--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,24 +1,62 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:food_app/pages/home/main_food_page.dart';
+
+import '../../utils/colors.dart';
+import '../../widgets/big_text.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
-}
-
-List pages = [
-  MainFoodPage(),
-  Container(child: Text("1st"),),
-  Container(child: Text("2nd"),),
-  Container(child: Text("3rd"),)
-
-];
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return pages[0];
   }
-}
+
+  class _HomePageState extends State<HomePage> {
+    int _selectedIndex = 0;
+    List pages = [
+      MainFoodPage(),
+      Container(child: Text("1st"),),
+      Container(child: Text("2nd"),),
+      Container(child: Text("3rd"),)
+
+    ];
+    void onTapBottomNav(int i){
+      setState(() {
+        _selectedIndex = i;
+      });
+    }
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        body: pages[_selectedIndex],
+        bottomNavigationBar: BottomNavigationBar(
+            unselectedItemColor: AppColors.mainColor,
+            unselectedLabelStyle: TextStyle(
+              color: AppColors.mainColor
+            ),
+            showUnselectedLabels: true,
+            currentIndex: _selectedIndex,
+            onTap: onTapBottomNav,
+            selectedItemColor: AppColors.iconColor1,
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: "Home"
+              ),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.archive),
+                  label: "Histórico"
+              ),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.shopping_cart),
+                  label: "Carrinho"
+              ),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.person),
+                  label: "Conta"
+              )
+        ])
+      );
+    }
+  }
